07cb9a1ed990ddabac021b0e042074177c708a89
Andrey Klimov <qwertyklim@mail.ru>
2024-02-28 20:36:15 +0700

    ошибки оформления

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 import { generatePosts } from './generate-posts.js';
 
-const Posts = generatePosts();
+const posts = generatePosts();
 
-//Posts[0].id = 30;
+//posts[0].id = 30;
 // eslint-disable-next-line no-console
-console.log(Posts);
+console.log(posts);
