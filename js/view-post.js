365bdb72e8efd8004fc3fa892aa188b72d2a0e12
Andrey Klimov <qwertyklim@mail.ru>
2024-03-07 22:22:22 +0700

    оптимизация. убрал часть выполненного от задания 8-2.

--- js/view-post.js
+++ js/view-post.js
@@ -1,5 +1,10 @@
 import { loadComments, clearComments } from './view-post-comments.js';
 
+const CommentsCount = {
+  ON_START: 30,
+  LOAD_MORE: 5,
+};
+
 const bigPicturePreviewElement = document.querySelector('.big-picture__preview');
 const imageElement = bigPicturePreviewElement.querySelector('.big-picture__img').querySelector('img');
 const socialElement = bigPicturePreviewElement.querySelector('.big-picture__social');
@@ -7,15 +12,34 @@ const likesCountElement = socialElement.querySelector('.likes-count');
 const commentShowCountElement = socialElement.querySelector('.social__comment-shown-count');
 const commentTotalCountElement = socialElement.querySelector('.social__comment-total-count');
 const captionElement = socialElement.querySelector('.social__caption');
+const commentsLoaderElement = document.querySelector('.comments-loader');
 
-const loadPost = ({ url, description, likes, comments }, commentsCount) => {
+//let currentComments;
+
+const onCommentsLoader = (/*evt*/) => {
+  //console.log(currentComments);
+};
+
+const loadPost = ({ url, description, likes, comments }) => {
+  //currentComments = comments;
   imageElement.src = url;
   imageElement.alt = description;
   likesCountElement.textContent = likes;
-  commentShowCountElement.textContent = commentsCount;
   commentTotalCountElement.textContent = comments.length;
-  loadComments(comments, commentsCount);
   captionElement.textContent = description;
+
+  const commentsStartCount = (CommentsCount.ON_START > comments.length) ? comments.length : CommentsCount.ON_START;
+  commentShowCountElement.textContent = commentsStartCount;
+  loadComments(comments, commentsStartCount);
+
+  if ((commentsStartCount >= 0) && (commentsStartCount < comments.length)) {
+    commentsLoaderElement.classList.remove('hidden');
+    commentsLoaderElement.addEventListener('click', onCommentsLoader);
+  } else {
+    if (!commentsLoaderElement.classList.contains('hidden')) {
+      commentsLoaderElement.classList.add('hidden');
+    }
+  }
 };
 
 const clearPost = () => {
@@ -24,8 +48,14 @@ const clearPost = () => {
   likesCountElement.textContent = '';
   commentShowCountElement.textContent = '';
   commentTotalCountElement.textContent = '';
-  clearComments();
   captionElement.textContent = '';
+
+  clearComments();
+
+  if (!commentsLoaderElement.classList.contains('hidden')) {
+    commentsLoaderElement.classList.add('hidden');
+    commentsLoaderElement.removeEventListener('click', onCommentsLoader);
+  }
 };
 
 export { clearPost, loadPost };
