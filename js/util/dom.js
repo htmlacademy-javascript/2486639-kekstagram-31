55bde97426e90c3dc9dba3ed08b604677f28c5ce
Andrey Klimov <qwertyklim@mail.ru>
2024-03-12 09:43:25 +0700

    докоммит

---
+++ js/util/dom.js
@@ -0,0 +1,20 @@
+/*
+const getTemplate = (id, className) => {
+  commentTemplate = commentsContainer.firstElementChild;
+  //if (commentTemplate) { throw new Error('commentTemplate не найден!'); }
+
+  return document.querySelector('#picture').content.querySelector('.picture');
+};
+
+const append = (dataElements, makeElement, container) => {
+  dataElements.forEach((dataElement) => {
+
+    //const element = commentTemplate.cloneNode(true);
+    const element = makeElement(dataElement)
+
+    //container.append(newCommentElement);
+  });
+
+  //container.append(...dataElements.map(makeElement));
+};
+*/
