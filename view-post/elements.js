402956e98317a6ed8276821f77ef019100ee51cc
Andrey Klimov <qwertyklim@mail.ru>
2024-03-25 09:52:53 +0700

    оформление

---
+++ view-post/elements.js
@@ -0,0 +1,36 @@
+import { addDot } from './../util/util.js';
+import { getFirstElementChild } from './../util/dom.js';
+
+const socialCommentsClass = 'social__comments'; //??
+const socialPictureClass = 'social__picture'; //??
+const socialTextClass = 'social__text'; //??
+const bigPictureElement = document.querySelector('.big-picture');
+const closePictureElement = bigPictureElement.querySelector('.big-picture__cancel');
+const commentsLoaderElement = bigPictureElement.querySelector('.comments-loader');
+const bigPicturePreviewElement = bigPictureElement.querySelector('.big-picture__preview');
+const imageElement = bigPicturePreviewElement.querySelector('.big-picture__img img');
+const socialElement = bigPicturePreviewElement.querySelector('.big-picture__social');
+const likesCountElement = socialElement.querySelector('.likes-count');
+const commentCountElement = socialElement.querySelector('.social__comment-count');
+const commentShowCountElement = commentCountElement.querySelector('.social__comment-shown-count');
+const commentTotalCountElement = commentCountElement.querySelector('.social__comment-total-count');
+const captionElement = socialElement.querySelector('.social__caption');
+const commentsContainerElement = document.querySelector(addDot(socialCommentsClass)); //??
+const commentTemplateElement = getFirstElementChild(socialCommentsClass); //??
+
+export {
+  socialCommentsClass,
+  socialPictureClass,
+  socialTextClass,
+  bigPictureElement,
+  closePictureElement,
+  commentsLoaderElement,
+  imageElement,
+  likesCountElement,
+  commentCountElement,
+  commentShowCountElement,
+  commentTotalCountElement,
+  captionElement,
+  commentsContainerElement,
+  commentTemplateElement
+};
