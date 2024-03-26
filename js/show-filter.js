f7e111b959291c721b7bf7d04f4daf319cc7874c
Andrey Klimov <qwertyklim@mail.ru>
2024-03-26 12:10:26 +0700

    применил замыкание

--- js/show-filter.js
+++ js/show-filter.js
@@ -1,30 +1,28 @@
 import { filterInactiveClass, filterButtonActiveClass, filtersContainerElement, filterButtonElements } from './elements.js';
 
-let onApplyFilter = null;
-
-const applyFilter = (evt) => {
-  const buttonElement = evt.currentTarget;
-  // проверка на активный фильтр
-  if (!buttonElement.classList.contains(filterButtonActiveClass)) {
-    // переместим выделение на нужную кнопку-фильтр
-    filterButtonElements.forEach((element) => {
-      element.classList.remove(filterButtonActiveClass);
-    });
-    buttonElement.classList.add(filterButtonActiveClass);
-    // вызовем нужную загрузку
-    if (onApplyFilter) {
-      onApplyFilter(buttonElement.id);
+const applyFilter = (onApplyFilter) =>
+  (evt) => {
+    const buttonElement = evt.currentTarget;
+    // проверка на активный фильтр
+    if (!buttonElement.classList.contains(filterButtonActiveClass)) {
+      // переместим выделение на нужную кнопку-фильтр
+      filterButtonElements.forEach((element) => {
+        element.classList.remove(filterButtonActiveClass);
+      });
+      buttonElement.classList.add(filterButtonActiveClass);
+      // вызовем нужную загрузку
+      if (onApplyFilter) {
+        onApplyFilter(buttonElement.id);
+      }
     }
-  }
-};
+  };
 
 const showFilter = (onChangeFilter) => {
-  onApplyFilter = onChangeFilter;
   filtersContainerElement.classList.remove(filterInactiveClass);
 
   filterButtonElements.forEach(
     (element) => {
-      element.addEventListener('click', applyFilter);
+      element.addEventListener('click', applyFilter(onChangeFilter));
     }
   );
 };
